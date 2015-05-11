d2979d52299501d88af6ab0c0abc08b8c9440333
princi <princi.scp8@gmail.com>
2015-05-11 13:41:08 +0530

    loops

---
+++ loop4.rb
@@ -0,0 +1,14 @@
+#number = 0
+
+#loop do
+ # break if number > 15
+#until number > 15 do
+#while number < 16 do
+#16.times do |number|
+#(0...15).each do |number|
+for number in 0..15 do
+puts "the number in the loop is #{number}"
+number += 1
+
+end
+
